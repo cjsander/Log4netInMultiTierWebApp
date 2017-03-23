391d5398043ad6b18654462680fb19d1c59f39b7
cjsander <chrystalsander@hotmail.com>
2017-03-23 16:24:11 -0500

    Initial commit I

---
+++ Log4netSample.Data/MediaData.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using log4net;
+using Log4netSample.Model;
+
+namespace Log4netSample.Data
+{
+    public class MediaData
+    {
+        private readonly ILog _logger = LogManager.GetLogger("LogDebug");
+        public List<Media> GetAllMedia()
+        {
+            _logger.Debug("Starting GetAllMedia.");
+            var listMedia = new List<Media>();
+
+            var media = new Media
+            {
+                Id = 1,
+                Title = "Halo: Guardians",
+                Genre = Genre.Shooter,
+                MediaType = MediaType.Game,
+                Platform = Platform.Xbox
+            };
+            ;
+            var media2 = new Media
+            {
+                Id = 2,
+                Title = "Les Miserables",
+                Genre = Genre.Fiction,
+                MediaType = MediaType.Print,
+                Platform = Platform.Book
+            };
+            var media3 = new Media
+            {
+                Id = 3,
+                Title = "Napoleon Dynamite",
+                Genre = Genre.Comedy,
+                MediaType = MediaType.DVD,
+                Platform = Platform.Movie
+            };
+            listMedia.Add(media);
+            listMedia.Add(media2);
+            listMedia.Add(media3);
+
+            _logger.Debug("Ending GetAllMedia");
+
+            return listMedia;
+
+        }
+
+        public Media GetMediaById(int searchId)
+        {
+            _logger.Debug("Starting GetMediaById.");
+            List<Media> listMedia = GetAllMedia();
+
+            IEnumerable<Media> media = from m in listMedia
+                where m.Id == searchId
+                select m;
+            _logger.Debug("Ending GetMediaById");
+            return media.FirstOrDefault();
+        }
+    }
+}
---
+++ Log4netSample.Model/Media.cs
@@ -0,0 +1,45 @@
+﻿namespace Log4netSample.Model
+{
+    public class Media  
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public MediaType MediaType { get; set; }
+        public Platform Platform { get; set; }
+        public Genre Genre { get; set; }
+     
+    }
+
+    public enum MediaType
+    {
+        Print,
+        CD,
+        DVD,
+        BlueRay,
+        Game
+
+    }
+
+    public enum Platform
+    {
+        Xbox,
+        Playstation,
+        Nintendo,
+        Pc,
+        Board,
+        Book,
+        Movie
+    }
+
+    public enum Genre
+    {
+        Comedy,
+        Fiction,
+        Biography,
+        Shooter,
+        ActionAdventure,
+        Rpg
+    }
+}
+
